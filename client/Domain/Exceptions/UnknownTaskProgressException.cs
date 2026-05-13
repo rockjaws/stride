ab3331cef7ddf54189df35a1aaf197a8dc387a50
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-05-13 13:21:07 +0200

    get tasks from selected project

---
+++ client/Domain/Exceptions/UnknownTaskProgressException.cs
@@ -0,0 +1,14 @@
+﻿using client.Domain.Enum;
+
+namespace client.Domain.Exceptions;
+
+public class UnknownTaskProgressException : Exception
+{
+    public TaskProgress Progress { get; }
+
+    public UnknownTaskProgressException(TaskProgress progress)
+        : base($"Unknown Task Progress Value: {progress}")
+    {
+        Progress = progress;
+    }
+}
