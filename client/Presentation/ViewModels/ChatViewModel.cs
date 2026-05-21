b322d09c1ebff2b392410fe8c0e144b920ad494f
Nicolaj Hansen <nicolaj_95@outlook.com>
2026-05-21 11:44:22 +0200

    chatvm setup

--- client/Presentation/ViewModels/ChatViewModel.cs
+++ client/Presentation/ViewModels/ChatViewModel.cs
@@ -1,3 +1,22 @@
+using System.Collections.ObjectModel;
+
+using client.Application.Interfaces;
+using client.Domain.Models;
+using client.Presentation.Common;
+
 namespace client.Presentation.ViewModels;
 
-public class ChatViewModel { }
+public class ChatViewModel : ObservableObject
+{
+    private readonly ILogger _logger;
+
+    // message service
+
+    private ObservableCollection<Message> _messages;
+    private ObservableCollection<ChatChannel> _chatChannels;
+
+    public ChatViewModel(ILogger logger)
+    {
+        _logger = logger;
+    }
+}
