5c2cbbe1db0c2b7df607388e527e7f3c9baffc21
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-05-26 12:57:08 +0200

    fix

--- api/Repositories/IProjectRepository.cs
+++ api/Repositories/IProjectRepository.cs
@@ -6,6 +6,7 @@ public interface IProjectRepository
 {
     Task<IEnumerable<Project>> GetAllProjectsAsync();
     Task<Project?> GetProjectByIdAsync(int id);
+    Task<IEnumerable<Project>> GetProjectsByUserIdAsync(int userId);
     Task AddProjectAsync(Project project);
     Task UpdateProjectAsync(Project project);
     Task DeleteProjectAsync(Project project);
